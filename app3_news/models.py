38b1dfa52e54f144fe35e3a1a259fa58ec7a20de
Ryulha <rhkong1018@gmail.com>
2025-05-21 13:35:20 +0900

    Revert "5/20"

--- app3_news/models.py
+++ app3_news/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Event(models.Model):
-    title = models.CharField(max_length=200) 
-    date = models.DateField() 
-    description = models.TextField(max_length=1000) 
-
-    def __str__(self):
-        return self.title
